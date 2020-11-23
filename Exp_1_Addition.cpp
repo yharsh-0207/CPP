af1e308ad378bb3740c5ca3ed912112cf8571540
yharsh-0207 <72005333+yharsh-0207@users.noreply.github.com>
2020-11-23 19:59:00 +0530

    Add files via upload

---
+++ Exp_1_Addition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+ int a,b,c;
+ cout<<"Enter two numbers:";
+ cin>>a>>b;
+ c=a+b;
+ cout<<"The sum of two numbers is:"<<c;
+ getch();
+}
